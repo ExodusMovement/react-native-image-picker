5679483ae5ad8461da8d07dfe87c10ce41ec391e
Vodopyanov Egor <egorvodopyanov@gmail.com>
2025-10-29 15:18:11 +0300

    feat: remove web support (#7)
    * remove web
    
    * update

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,41 @@
 import {Platform} from 'react-native';
 
-import {CameraOptions, ImageLibraryOptions, Callback} from './types';
+import {
+  CameraOptions,
+  ImageLibraryOptions,
+  Callback,
+  ImagePickerResponse,
+} from './types';
 import {
   imageLibrary as nativeImageLibrary,
   camera as nativeCamera,
 } from './platforms/native';
-import {
-  imageLibrary as webImageLibrary,
-  camera as webCamera,
-} from './platforms/web';
 
 export * from './types';
 
 export function launchCamera(options: CameraOptions, callback?: Callback) {
-  return Platform.OS === 'web'
-    ? webCamera(options, callback)
-    : nativeCamera(options, callback);
+  if (Platform.OS === 'web') {
+    const result: ImagePickerResponse = {
+      errorCode: 'others',
+      errorMessage: 'Web platform is not supported',
+    };
+    if (callback) callback(result);
+    return Promise.resolve(result);
+  }
+  return nativeCamera(options, callback);
 }
 
 export function launchImageLibrary(
   options: ImageLibraryOptions,
   callback?: Callback,
 ) {
-  return Platform.OS === 'web'
-    ? webImageLibrary(options, callback)
-    : nativeImageLibrary(options, callback);
+  if (Platform.OS === 'web') {
+    const result: ImagePickerResponse = {
+      errorCode: 'others',
+      errorMessage: 'Web platform is not supported',
+    };
+    if (callback) callback(result);
+    return Promise.resolve(result);
+  }
+  return nativeImageLibrary(options, callback);
 }
